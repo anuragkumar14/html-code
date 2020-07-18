1384bde84289aeba26d5f77e8f2d628512cdc3bd
Anurag Kumar <anurag991012@gmail.com>
2020-07-18 23:33:43 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
-<body bgcolor='aqua'>
-<img src='https://media-cdn.mclaren.com/media/images/downloads/wallpapers/1920x1200/AUSLANDO_1920X1200.jpg' height='1920' width='1200' />
+<body bgcolor='#FFA500'>
+  <?php
+print "Mclaren F1 Formula One Team"
+?>
+</pre
+<img src='https://media-cdn.mclaren.com/media/images/downloads/wallpapers/1920x1200/AUSLANDO_1920X1200.jpg' height='1600' width='720' />
 <pre>
 <?php
 print "Mclaren F1 Formula One Team"
